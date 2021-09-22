6a25b14c70026c1b2c6bf30825ca9f913f67628b
devnow8 <72devdev@gmail.com>
2021-09-22 22:43:31 +0900

    footer

---
+++ main.js
@@ -0,0 +1,10 @@
+var lnb = $("#lnb").offset().top;
+$(window).scroll(function() {
+  	var window = $(this).scrollTop();
+
+    if(lnb <= window) {
+      $("#lnb").addClass("fixed");
+    } else {
+      $("#lnb").removeClass("fixed");
+    }
+})
