6c9866d2fddce3f22eabffe245673cc9f4fa82d6
devnow8 <72devdev@gmail.com>
2021-09-23 02:53:14 +0900

    header-sticky

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-var lnb = $("#lnb").offset().top;
-$(window).scroll(function() {
-  	var window = $(this).scrollTop();
+// var lnb = $("#lnb").offset().top;
+// $(window).scroll(function() {
+//   	var window = $(this).scrollTop();
 
-    if(lnb <= window) {
-      $("#lnb").addClass("fixed");
-    } else {
-      $("#lnb").removeClass("fixed");
-    }
-})
+//     if(lnb <= window) {
+//       $("#lnb").addClass("fixed");
+//     } else {
+//       $("#lnb").removeClass("fixed");
+//     }
+// })
+
+$(window).scroll(function() {
+	var scroll = $(window).scrollTop();
+	console.log(scroll);
+	if (scroll >= 50) {
+		console.log('a');
+		$(".header").addClass("change");
+	} else {
+		console.log('a');
+		$(".header").removeClass("change");
+	}
+});
