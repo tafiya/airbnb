bb8e4f02688f37ed4901c7645c179ee6d831f8ad
devnow8 <72devdev@gmail.com>
2021-09-29 00:45:00 +0900

    dark mode

--- main.js
+++ main.js
@@ -9,14 +9,28 @@
 //     }
 // })
 
-$(window).scroll(function() {
-	var scroll = $(window).scrollTop();
-	console.log(scroll);
-	if (scroll >= 50) {
-		console.log('a');
-		$(".header").addClass("change");
-	} else {
-		console.log('a');
-		$(".header").removeClass("change");
-	}
-});
+// $(window).scroll(function() {
+// 	var scroll = $(window).scrollTop();
+// 	console.log(scroll);
+// 	if (scroll >= 50) {
+// 		console.log('a');
+// 		$(".header").addClass("change");
+// 	} else {
+// 		console.log('a');
+// 		$(".header").removeClass("change");
+// 	}
+// });
+
+  // var icon = document.getElementById("icon");
+  var icon = document.querySelector(".dark-mode");
+    
+  icon.onClick = function(){
+    window.alert("dark");
+      // document.body.classList.toggle("dark-theme");
+      // if(document.body.classList.contains("dark-theme")) {
+      //     icon.src = "images/day-mode.png"
+      // }else{
+      //     icon.src = "images/dark-mode.png"
+          
+      // }
+  }
